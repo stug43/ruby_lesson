2464d3ffa877b090b19ddd7c7d90d807ff3f72e6
stug43 <nganta.a@hotmail.fr>
2018-12-01 22:58:23 +0100

    ajout de 6 exercices

---
+++ exo_10.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, quel âge avez vous ?"
+user_age = gets.chomp.to_i
+puts "En 2017, vous aviez #{2017-user_age} ans."
---
+++ exo_12.rb
@@ -0,0 +1,8 @@
+puts "Entrez un entier naturel"
+print ">"
+final_count = gets.chomp.to_i
+i=1
+until i == final_count do
+  puts final_count
+  i += 1
+end
