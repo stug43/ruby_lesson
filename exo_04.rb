e119dcdaa12b5fefc6a1f9c523132fa468eee7e0
stug43 <nganta.a@hotmail.fr>
2018-12-02 21:48:03 +0100

    modif commentaires, en ruby on utilise =begin =end à la place de /* */

--- exo_04.rb
+++ exo_04.rb
@@ -1,4 +1,6 @@
-/* Lors de l'interprétation la machine remarque que la syntaxe de ruby n'est pas respectée 
-et retourne un message d'erreur (unterminated string meets end of file) */
+=begin 
+Lors de l'interprétation la machine remarque que la syntaxe de ruby n'est pas 
+respectée et retourne un message d'erreur (unterminated string meets end of file)
+=end
 
 puts "Salut, ça farte ?
